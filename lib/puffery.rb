ec4e1e471c3c780bba5226b140ac0afcbf1291f3
Gustav <gustav@invoke.se>
2015-07-11 11:47:24 +0200

    Use module_function in favour of class << self

--- lib/puffery.rb
+++ lib/puffery.rb
@@ -7,36 +7,34 @@ require 'puffery/builder'
 
 module Puffery
 
-  class << self
+  module_function
 
-    def configuration
-      @configuration ||= Configuration.new
-    end
+  def configuration
+    @configuration ||= Configuration.new
+  end
 
-    def configure
-      yield configuration
-    end
+  def configure
+    yield configuration
+  end
 
-    def load_files
-      files_in_load_path.each { |file| require_dependency(file) }
-    end
+  def load_files
+    files_in_load_path.each { |file| require_dependency(file) }
+  end
 
-    def files_in_load_path
-      configuration.load_paths.flatten.compact.uniq.map do |path|
-        Dir["#{path}/**/*.rb"]
-      end.flatten
-    end
+  def files_in_load_path
+    configuration.load_paths.flatten.compact.uniq.map do |path|
+      Dir["#{path}/**/*.rb"]
+    end.flatten
+  end
 
-    def register(name, &block)
-      Namespace.add(name, &block)
-    end
+  def register(name, &block)
+    Namespace.add(name, &block)
+  end
 
-    def build_payload(resource, ns_name = nil)
-      ns_name ||= resource.namespace
-      namespace = Namespace.find(ns_name)
-      Puffery::Builder.build(resource, namespace)
-    end
-
+  def build_payload(resource, ns_name = nil)
+    ns_name ||= resource.namespace
+    namespace = Namespace.find(ns_name)
+    Puffery::Builder.build(resource, namespace)
   end
 
 end
