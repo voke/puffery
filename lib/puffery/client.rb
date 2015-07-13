2e61903cc742d4740708b6bebe68890f60184bc5
Gustav <gustav@invoke.se>
2015-07-13 12:58:11 +0200

    Add up request and handle errors

--- lib/puffery/client.rb
+++ lib/puffery/client.rb
@@ -1,7 +1,11 @@
+require 'excon'
+require 'json'
+
 module Puffery
   class Client
 
     USER_AGENT = "Puffery Ruby #{Puffery::VERSION}"
+    RequestError = Class.new(StandardError)
 
     attr_accessor :url, :key
 
@@ -15,14 +19,34 @@ module Puffery
     end
 
     def conn
-      @conn = Excon.new(self.url, debug: true,
-        headers: { 'Authorization' => auth_header })
+      @conn ||= Excon.new(self.url, debug: true, headers:
+        { 'Content-Type' => 'application/json' })
+    end
+
+    def up(resource)
+      payload = Puffery.build_payload(resource)
+      payload[:ad_group][:status] = 'enabled'
+      uid = resource.remote_uid
+      raise 'Missing UID' unless uid
+      response = conn.put(path: "/api/ad_groups/#{resource.remote_uid}",
+        body: JSON.dump(payload)
+      )
+      handle_errors(response)
+      response
+    end
+
+    def handle_errors(response)
+      data = JSON.parse(response.body)
+      if data['errors'].any?
+        raise RequestError,
+          "Request Error occurred: %s" % data['errors'].first
+      end
     end
 
     # Update partial resources
     def patch(payload)
     end
-    
+
     # PUT request replace whole object
     # PUTting the same data multiple times to the same resource
     # should not result in different resources
